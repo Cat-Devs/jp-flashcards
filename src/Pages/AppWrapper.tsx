673e74fcc909a6de05a6c13232eb324b2b311bbd
andreasonny83 <andreasonny83@gmail.com>
2022-02-06 09:30:48 +0000

    Fix app menu

--- src/Pages/AppWrapper.tsx
+++ src/Pages/AppWrapper.tsx
@@ -5,11 +5,11 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 
 import { theme } from "../theme";
 import { useApp } from "../AppState";
+import { Container } from "@mui/material";
 
 export const AppWrapper = (props) => {
   const { goHome } = useApp();
@@ -17,13 +17,13 @@ export const AppWrapper = (props) => {
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="static">
-        <Container maxWidth="xl">
+        <Container>
           <Toolbar disableGutters>
-            <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", md: "flex" } }}>
-              JPFlashCards
+            <Typography variant="h6" noWrap component="div" sx={{ mr: 2, display: { xs: "none", sm: "flex" } }}>
+              JP-FlashCardS
             </Typography>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Box sx={{ flexGrow: 1, display: { display: { sm: "flex" }, justifyContent: "end" } }}>
               <Button variant="text" onClick={goHome} sx={{ my: 2, color: "white", display: "block" }}>
                 Home
               </Button>

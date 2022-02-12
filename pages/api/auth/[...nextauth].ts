2be68c6d873619530343098f2d47424f3b7f1fd1
andreasonny83 <andreasonny83@gmail.com>
2022-02-12 16:36:52 +0000

    Dev authentication

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,24 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+const isDev = Boolean(process.env.DEV);
+
+const devProviders = [];
+
+if (isDev) {
+  devProviders.push(
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize() {
+        return { id: 1, name: "J Smith", email: "jsmith@example.com" };
+      },
+    })
+  );
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -8,6 +27,7 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
     }),
+    ...devProviders,
   ],
   theme: {
     colorScheme: "dark",

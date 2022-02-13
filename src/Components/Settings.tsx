71bf5519d90483ee6be35ad8c8faaccd857c795c
andreasonny83 <andreasonny83@gmail.com>
2022-02-13 13:59:30 +0000

    Add level 5

--- src/Components/Settings.tsx
+++ src/Components/Settings.tsx
@@ -56,6 +56,7 @@ export const Settings: React.FC = () => {
           <FormControlLabel value="2" control={<Radio />} label="2" />
           <FormControlLabel value="3" control={<Radio />} label="3" />
           <FormControlLabel value="4" control={<Radio />} label="4" />
+          <FormControlLabel value="5" control={<Radio />} label="5" />
         </RadioGroup>
       </FormControl>
     </Box>

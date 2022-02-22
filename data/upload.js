76e46a7c31f6e957ee554382225c5378fea25e94
andreasonny83 <andreasonny83@gmail.com>
2022-02-22 17:16:29 +0000

    Upload CLI upload only one file

--- data/upload.js
+++ data/upload.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
 const path = require('path');
+const yargs = require('yargs');
 
 AWS.config.update({
   region: process.env.AWS_REGION,
@@ -15,6 +16,17 @@ function padLeft(input, outputLength) {
 }
 
 (async () => {
+  const argv = yargs
+    .option('category', {
+      year: {
+        description: 'Upload a single category file',
+        alias: 'y',
+        type: 'string',
+      },
+    })
+    .help()
+    .alias('help', 'h').argv;
+
   const dynamoDb = new AWS.DynamoDB.DocumentClient({
     region: process.env.AWS_REGION,
     params: { TableName },
@@ -26,24 +38,27 @@ function padLeft(input, outputLength) {
 
   for (const [fileId, file] of files.entries()) {
     const tableData = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db', file), 'utf8'));
-    console.log(`Uploading ${file}...`);
-    for (const data of tableData) {
-      const cardId = `1${padLeft(String(fileId + 1), 2)}${padLeft(String(data.id), 3)}`;
-      const params = {
-        TableName,
-        Item: {
-          ...data,
-          id: String(cardId),
-          type: 'card',
-        },
-      };
 
-      try {
-        await dynamoDb.put(params).promise();
-      } catch (error) {
-        console.error('failed to put', data.en);
-        console.error(error);
-        process.exit(1);
+    if (argv.category && `${argv.category}.json` === file) {
+      console.log(`Uploading ${file}...`);
+      for (const data of tableData) {
+        const cardId = `1${padLeft(String(fileId + 1), 2)}${padLeft(String(data.id), 3)}`;
+        const params = {
+          TableName,
+          Item: {
+            ...data,
+            id: String(cardId),
+            type: 'card',
+          },
+        };
+
+        try {
+          await dynamoDb.put(params).promise();
+        } catch (error) {
+          console.error('failed to put', data.en);
+          console.error(error);
+          process.exit(1);
+        }
       }
     }
   }

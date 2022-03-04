8995b54a49ac32386ccde1ac068819e6c685f38a
andreasonny83 <andreasonny83@gmail.com>
2022-03-04 14:57:02 +0000

    Test release version

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,11 +4,12 @@ import React from 'react';
 import { Copyright } from './Copyright';
 
 export const Footer = () => {
+  console.log(process.env);
   return (
     <Box py={1}>
       <Copyright />
       <Typography variant="body2" color="textSecondary" align="center">
-        release version <Typography variant="overline">{process.env.VERCEL_GIT_COMMIT_SHA || 'dev'}</Typography>
+        release version <Typography variant="overline">{process.env.REACT_APP_GIT_HASH || 'dev'}</Typography>
       </Typography>
     </Box>
   );

3fcddf41824e621821dfaacb8a5fc008aefb6138
andreasonny83 <andreasonny83@gmail.com>
2022-03-04 15:44:31 +0000

    Shorter release version in footer

--- pages/api/version.ts
+++ pages/api/version.ts
@@ -1,7 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+const { VERCEL_GIT_COMMIT_SHA } = process.env;
 
 const getVersion = async (_req: NextApiRequest, res: NextApiResponse) => {
-  res.json({ data: process.env.VERCEL_GIT_COMMIT_SHA || 'dev' });
+  const version = VERCEL_GIT_COMMIT_SHA && VERCEL_GIT_COMMIT_SHA.split('').splice(0, 7).join('');
+  res.json({ data: version || 'dev' });
 };
 
 export default getVersion;
